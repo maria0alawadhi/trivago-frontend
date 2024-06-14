730889f9fd821627a468e694860eaea8c291a247
maria0alawadhi <mariaalawadhi@gmail.com>
2024-06-14 16:41:12 +0300

    create routes ,Header,SignIn and Register

--- client/src/pages/Reservations.jsx
+++ client/src/pages/Reservations.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import '../App.css'
 
 const Reservations = () => {
   return <div></div>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,59 @@
-import './App.css'
+import '../App.css'
 
-const SignIn = () => {
-  return <div></div>
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { SignInUser } from '../services/Auth'
+
+const SignIn = ({ setUser }) => {
+  let navigate = useNavigate()
+  
+  const [formValues, setFormValues] = useState({ email: '', password: '' })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = await SignInUser(formValues)
+    setFormValues({ email: '', password: '' })
+    setUser(payload)
+    navigate('/')
+  }
+
+  return (
+    <div className="container wrapper">
+      <div className="signin-card">
+        <form onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              name="email"
+              type="email"
+              placeholder="example@example.com"
+              value={formValues.email}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <button disabled={!formValues.email || !formValues.password}>
+            Sign In
+          </button>
+        </form>
+      </div>
+      </div>
+
+  )
 }
 
 export default SignIn
