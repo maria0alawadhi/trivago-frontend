6bc12d69f99382d463936ba859079b06ae40d67f
user <amal2001.jalil@gmail.com>
2024-06-15 03:06:39 +0300

    made hotels and rooms pages

--- client/src/components/RoomCard.jsx
+++ client/src/components/RoomCard.jsx
@@ -1,7 +1,19 @@
-import './App.css'
+import '../App.css'
 
-const RoomCard = () => {
-  return <div></div>
+const RoomCard = (props) => {
+  return (
+    <div className="room-card " id={props.id}>
+      <div className="img-wrapper">
+        <img src={props.img} />
+      </div>
+      <div className="info-wrapper">
+        <h3>{props.name}</h3>
+        <h4>Type : {props.type}</h4>
+        <h5>Price : {props.price}</h5>
+        <p>Rating : {props.rating} stars</p>
+      </div>
+    </div>
+  )
 }
 
 export default RoomCard
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import { useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import '../App.css'
 import axios from 'axios'
-
-
+import HotelCard from '../components/HotelCard'
+import { Link } from 'react-router-dom'
 const Home = () => {
   const [hotels, sethotels] = useState([])
 
@@ -19,15 +19,23 @@ const Home = () => {
     hotels()
   }, [])
 
-  return (<div>
-      {
-        hotels.map((hotel,index)=>(
-          <div className="small-card" key={index}>
-            {hotel.name}
-          </div>
-        ))
-      }
-  </div>)
+  return (
+    <div className="Home-card">
+      <h2>Hotels</h2>
+      <div className="hotels">
+        {hotels.map((hotel, index) => (
+          <Link to={`/hotels/${hotel._id}/rooms`} key={index} className="link">
+            <HotelCard
+              id={index}
+              img={hotel.img}
+              name={hotel.name}
+              location={hotel.location}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Home
