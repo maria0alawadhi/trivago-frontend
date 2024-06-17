23af43ae198f16511a7383993bcd375d60a53afe
maria0alawadhi <mariaalawadhi@gmail.com>
2024-06-17 23:35:58 +0300

    create reservation Component + add the logic for checking the current user reservation + delete reservation is done

--- client/src/components/ReservationCard.jsx
+++ client/src/components/ReservationCard.jsx
@@ -1,7 +1,49 @@
-import './App.css'
+import '../App.css'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-const ReservationCard = () => {
-  return <div></div>
+const ReservationCard = ({ reservations }) => {
+  let navigate = useNavigate()
+  const checkIn = reservations.checkIn
+  const checkOut = reservations.checkOut
+  const checkInDate = checkIn.slice(0, 10)
+  const checkOutDate = checkOut.slice(0, 10)
+
+  const handleDelete = async () => {
+    if (window.confirm('Are you sure you want to delete this reservation?')) {
+      try {
+        await axios.delete(
+          `http://localhost:3001/reservations/${reservations._id}`
+        )
+        navigate('/')
+      } catch (error) {
+        console.error('Error deleting a reservation', error)
+      }
+    }
+  }
+
+  return (
+    <div className="reservation-card">
+      <div>
+        {/* <img src={} alt="img" />  */} {/* Placeholder for image  */}
+      </div>
+      <div>
+        <p className="check-in">
+          <b>Check-In Date:</b> {checkInDate}
+        </p>
+        <p className="check-out">
+          <b>Check-Out Date:</b> {checkOutDate}
+        </p>
+      </div>
+      <div>
+        <button className="btn btn-edit">Edit</button>
+        <button className="btn btn-delete" onClick={handleDelete}>
+          Delete
+        </button>
+        <button className="btn btn-review">Review</button>
+      </div>
+    </div>
+  )
 }
 
 export default ReservationCard
--- client/src/pages/Reservations.jsx
+++ client/src/pages/Reservations.jsx
@@ -1,7 +1,48 @@
+import React, { useEffect, useState } from 'react'
 import '../App.css'
+import ReservationCard from '../components/ReservationCard'
+import axios from 'axios'
 
-const Reservations = () => {
-  return <div></div>
+const Reservations = ({ user }) => {
+  const [userReservations, setUserReservations] = useState([])
+
+  useEffect(() => {
+    const fetchUserReservations = async () => {
+      try {
+        if (user) {
+          const response = await axios.get(
+            `http://localhost:3001/reservations?user=${user._id}`
+          )
+          setUserReservations(response.data)
+        }
+      } catch (error) {
+        console.error('Error Data Fetching:', error)
+      }
+    }
+
+    fetchUserReservations()
+  }, [user])
+
+  const currentUserReservation = userReservations.filter(
+    (reservation) => reservation.user.toString() === user.id
+  )
+
+  console.log('user' + JSON.stringify(user))
+
+  console.log('reservation' + JSON.stringify(userReservations))
+  console.log(currentUserReservation)
+
+  return (
+    <div>
+      {user ? (
+        currentUserReservation.map((reservation) => (
+          <ReservationCard key={reservation._id} reservations={reservation} />
+        ))
+      ) : (
+        <h3>No Reservations yet</h3>
+      )}
+    </div>
+  )
 }
 
 export default Reservations
