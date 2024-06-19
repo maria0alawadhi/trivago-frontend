ff3a41300171f3cf4600a8935885e9885100a2f9
maria0alawadhi <mariaalawadhi@gmail.com>
2024-06-19 13:53:25 +0300

    adding review component

---
+++ client/src/pages/Review.jsx
@@ -0,0 +1,14 @@
+const Review = () => {
+  //post review&rating
+  return (
+    <div>
+      <div className="rating-container">
+        <form action=""></form>
+      </div>
+      <div className="review-container">
+        <input type="text" />
+      </div>
+    </div>
+  )
+}
+export default Review
