28e034faa2cbd48e8ba0372ebd3cd621e03c6b0c
user <amal2001.jalil@gmail.com>
2024-06-20 07:11:19 +0300

    added navgation to confim

--- client/src/pages/ReviewForm.jsx
+++ client/src/pages/ReviewForm.jsx
@@ -3,14 +3,14 @@ import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-const ReviewForm = ({user}) => {
-const { roomid } = useParams();
-const userid=user.id
+const ReviewForm = ({ user }) => {
+  const { roomid } = useParams()
+  const userid = user.id
   const [review, setReview] = useState({
     review: '',
     rating: 0,
-    user:userid,
-    room:roomid
+    user: userid,
+    room: roomid
   })
 
   const handleChange = (event) => {
@@ -32,14 +32,13 @@ const userid=user.id
     }
   }
 
-
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
         <label htmlFor="room">RoomId "for reference only":</label>
         <input type="text" name="room" value={review.room} disabled />
         <label htmlFor="user">UserId "for reference only":</label>
-        <input type="text" name="user" value={review.user}disabled />
+        <input type="text" name="user" value={review.user} disabled />
         <label htmlFor="review">Review:</label>
         <textarea name="review" value={review.review} onChange={handleChange} />
         <label htmlFor="rating">Rating:</label>
@@ -53,8 +52,6 @@ const userid=user.id
         />
         <button type="submit">Submit Review</button>
       </form>
-   
-      
     </div>
   )
 }
