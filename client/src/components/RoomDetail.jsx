7e3cb6da2341555b213a62daf1947dbedf9abeed
user <amal2001.jalil@gmail.com>
2024-06-20 15:56:55 +0300

    added style

--- client/src/components/RoomDetail.jsx
+++ client/src/components/RoomDetail.jsx
@@ -18,54 +18,55 @@ const RoomDetail = ({
 
   return (
     <div className="room-detail" id={room.id}>
-      <img src={room.img} alt={room.name} />
-      <div className="room-data">
-        <h3>Room name: {room.name}</h3>
-        <h4>Type: {room.type}</h4>
-        <h5>Price: {room.price} BD</h5>
+      <div className="flex-card">
+        <img src={room.img} alt={room.name} />{' '}
+        {user && (
+          <div className="container2">
+            <div>
+              <h4>Reserve Your Room</h4>
+            </div>
+            <form onSubmit={handleFormSubmit} className="reservation">
+              <label htmlFor="checkIn" className="checkout">
+                Check In:
+                <input
+                  className="checkout"
+                  type="date"
+                  value={checkIn}
+                  name="checkIn"
+                  onChange={(e) => {
+                    console.log('Check-in date selected:', e.target.value)
+                    setCheckIn(e.target.value)
+                  }}
+                  required
+                />
+              </label>
+              <label htmlFor="checkOut" className="checkout">
+                Check Out:
+                <input
+                  className="checkout"
+                  type="date"
+                  value={checkOut}
+                  onChange={(e) => {
+                    console.log('Check-out date selected:', e.target.value)
+                    setCheckOut(e.target.value)
+                  }}
+                  required
+                />
+              </label>
 
+              <button type="submit" className="submit">
+                Confirm
+              </button>
+            </form>
+          </div>
+        )}
+      </div>
+      <div className="data-room">
+        <h4>Room name: {room.name}</h4>
+        <h4>Type: {room.type}</h4>
+        <h4>Price: {room.price} BD</h4>
         <h5>Facilities: {room.facilities.join(', ')}</h5>
       </div>
-      {user && (
-        <div className="container2">
-          <div>
-            <h4>Reserve Your Room</h4>
-          </div>
-          <form onSubmit={handleFormSubmit} className="reservation">
-            <label htmlFor="checkIn" className="checkout">
-              Check In:
-              <input
-                className="checkout"
-                type="date"
-                value={checkIn}
-                name="checkIn"
-                onChange={(e) => {
-                  console.log('Check-in date selected:', e.target.value)
-                  setCheckIn(e.target.value)
-                }}
-                required
-              />
-            </label>
-            <label htmlFor="checkOut" className="checkout">
-              Check Out:
-              <input
-                className="checkout"
-                type="date"
-                value={checkOut}
-                onChange={(e) => {
-                  console.log('Check-out date selected:', e.target.value)
-                  setCheckOut(e.target.value)
-                }}
-                required
-              />
-            </label>
-
-            <button type="submit" className="submit">
-              Confirm
-            </button>
-          </form>
-        </div>
-      )}
       <h5>Review: {room.review}</h5>
     </div>
   )
